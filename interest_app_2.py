e60aaf4ac4a5d233e6b86c6183d07696ce1769f5
Chunyun Tu <yidapa@yeah.net>
2016-01-23 16:55:17 +0800

    modified:   interest_app_2.py

--- interest_app_2.py
+++ interest_app_2.py
@@ -7,8 +7,8 @@ Created on Thu Jan 21 22:15:44 2016
 
 from interest import days
 # How many days does it take to double an amount when the
-# interest rate is p=1,2,3,...14?
-for p in range(1, 15):
+# interest rate is p=1,2,3,...20?
+for p in range(1, 21):
     years = days(1, 2, p)/365.0
     print( 'With p=%2d%% it takes %4.1f years to double the amount' %\
     (p, years) )
